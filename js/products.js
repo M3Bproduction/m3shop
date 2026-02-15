cb899150f781d243b31e3c333a8fec319fbeef97
M3Bproduction <madjidchakira@gmail.com>
2026-02-15 22:21:34 +0300

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    id: 1,
+    name: "Redmi 8A",
+    price: 29500,
+    image: "images/redmi8a.jpg"
+  },
+  {
+    id: 2,
+    name: "iPhone XR comme 17",
+    price: 112500,
+    image: "images/iphonexr17.jpg"
+  }
+];
