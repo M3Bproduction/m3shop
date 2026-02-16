bdf718505f9e0cd84312f4966b2bad8013f7fa5b
M3Bproduction <madjidchakira@gmail.com>
2026-02-16 09:17:02 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,12 +3,14 @@ const products = [
     id: 1,
     name: "Redmi 8A",
     price: 29500,
-    image: "images/redmi8a.jpg"
+    image: "images/redmi8a.jpg",
+    category: "Téléphone"
   },
   {
     id: 2,
     name: "iPhone XR comme 17",
     price: 112500,
-    image: "images/iphonexr17.jpg"
+    image: "images/iphonexr17.png",
+    category: "Téléphone"
   }
 ];
