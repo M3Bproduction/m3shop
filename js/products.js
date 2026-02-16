a2af65e4d0163f16e8dd6c12d4f7da322c576785
M3Bproduction <madjidchakira@gmail.com>
2026-02-16 21:50:37 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,9 +1,23 @@
 const products = [
   {
-    id: 1,
-    name: "Redmi 8A",
-    price: 29500,
-    image: "images/redmi8a.jpg",
+    id: 5,
+    name: "Infinix smart 10hd",
+    price: 57500,
+    image: "images/inf10hd.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 6,
+    name: "iPhone 11",
+    price: 92500,
+    image: "images/iphone11.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 7,
+    name: "iPhone XR",
+    price: 87500,
+    image: "images/iphonexr.jpg",
     category: "Téléphone"
   },
   {
@@ -12,5 +26,131 @@ const products = [
     price: 112500,
     image: "images/iphonexr17.png",
     category: "Téléphone"
-  }
+  },
+  {
+    id: 8,
+    name: "Nokia C2",
+    price: 30000,
+    image: "images/nokiac2.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 1,
+    name: "Redmi 8A",
+    price: 29500,
+    image: "images/redmi8a.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 9,
+    name: "Redmi 9A",
+    price: 42500,
+    image: "images/redmi9a.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 10,
+    name: "Redmi 9C",
+    price: 37500,
+    image: "images/redmi9c.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 11,
+    name: "Redmi 12",
+    price: 72500,
+    image: "images/redmi12.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 12,
+    name: "Redmi 14C (128Go)",
+    price: 77500,
+    image: "images/redmi14c.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 13,
+    name: "Redmi 14C (256Go)",
+    price: 82500,
+    image: "images/redmi14c2.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 14,
+    name: "Redmi A2+",
+    price: 50500,
+    image: "images/redmia2plus.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 15,
+    name: "Redmi A5",
+    price: 57500,
+    image: "images/redmia5.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 16,
+    name: "Redmi Note 13 Pro",
+    price: 112500,
+    image: "images/redminote13.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 3,
+    name: "Samsung Galaxy A03 Core",
+    price: 50000,
+    image: "images/smsga03.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 4,
+    name: "Samsung Galaxy A3 Core",
+    price: 30500,
+    image: "images/smsga3.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 17,
+    name: "Samsung Galaxy A15",
+    price: 82500,
+    image: "images/smsga15.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 18,
+    name: "Samsung Galaxy A16",
+    price: 82500,
+    image: "images/smsga16.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 19,
+    name: "Samsung A037u",
+    price: 37500,
+    image: "images/smsga037.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 20,
+    name: "Samsung Galaxy F13",
+    price: 67500,
+    image: "images/smsgf13.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 21,
+    name: "Samsung Galaxy M05",
+    price: 57500,
+    image: "images/smsgm05.jpg",
+    category: "Téléphone"
+  },
+  {
+    id: 22,
+    name: "Wiko T10",
+    price: 37500,
+    image: "images/wikot10.jpg",
+    category: "Téléphone"
+  },
 ];
